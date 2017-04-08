587d623652b026796be1595efc1c37f565fa94fe
Greg Althaus <galthaus@austin.rr.com>
2017-04-08 18:24:10 -0500

    First pass at auto-versioning - more to come.

--- version.go
+++ version.go
@@ -1,7 +1,11 @@
 package provision
 
-const RS_VERSION = "v3.0.1-pre-alpha"
 const RS_MAJOR_VERSION = "3"
 const RS_MINOR_VERSION = "0"
-const RS_PATCH_VERSION = "1"
+const RS_PATCH_VERSION = "2"
 const RS_EXTRA = "-pre-alpha"
+
+var GitHash = "NotSet"
+var BuildStamp = "Not Set"
+
+var RS_VERSION = "v" + RS_MAJOR_VERSION + "." + RS_MINOR_VERSION + "." + RS_PATCH_VERSION + RS_EXTRA + "-" + GitHash
