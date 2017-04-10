05e8e9fe3ee7f7f27b9a61c671b483bd4b6f2f59
Greg Althaus <galthaus@austin.rr.com>
2017-04-09 22:03:19 -0500

    Make the version 100% built.

--- version.go
+++ version.go
@@ -1,11 +1,10 @@
 package provision
 
-const RS_MAJOR_VERSION = "3"
-const RS_MINOR_VERSION = "0"
-const RS_PATCH_VERSION = "2"
-const RS_EXTRA = "-pre-alpha"
-
+var RS_PREPART = "v"
+var RS_MAJOR_VERSION = "3"
+var RS_MINOR_VERSION = "0"
+var RS_PATCH_VERSION = "2"
+var RS_EXTRA = "-pre-alpha"
 var GitHash = "NotSet"
 var BuildStamp = "Not Set"
-
-var RS_VERSION = "v" + RS_MAJOR_VERSION + "." + RS_MINOR_VERSION + "." + RS_PATCH_VERSION + RS_EXTRA + "-" + GitHash
+var RS_VERSION = RS_PREPART + RS_MAJOR_VERSION + "." + RS_MINOR_VERSION + "." + RS_PATCH_VERSION + RS_EXTRA + "-" + GitHash
