1a5bf9b0a1789e7494db0d63fc82e8fc44835e62
Greg Althaus <galthaus@austin.rr.com>
2017-06-28 05:20:19 +0000

    Undo the exit 1 in local.  It isn't needed, it was a console setting issue with packet.

--- cli/template_test.go
+++ cli/template_test.go
@@ -15,7 +15,7 @@ var templateDefaultListString string = `[
     "ID": "local-elilo.tmpl"
   },
   {
-    "Contents": "#!ipxe\nexit 1\n",
+    "Contents": "#!ipxe\nexit\n",
     "ID": "local-ipxe.tmpl"
   },
   {
@@ -72,7 +72,7 @@ var templateListBothEnvsString = `[
     "ID": "local-elilo.tmpl"
   },
   {
-    "Contents": "#!ipxe\nexit 1\n",
+    "Contents": "#!ipxe\nexit\n",
     "ID": "local-ipxe.tmpl"
   },
   {
