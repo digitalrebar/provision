6d779baaf20a208fe0e84575a0315e98bd82995f
Victor Lowther <victor.lowther@gmail.com>
2017-08-23 10:35:42 -0500

    Add /indexes endpoints to the frontend.
    Rejiggering the CLI to use them instead of cheating and importing the backend
    models to use them directly will take more work.

---
+++ models/index.go
@@ -0,0 +1,12 @@
+package models
+
+// Index holds details on the index
+// swagger:model
+type Index struct {
+	// Type gives you a rough idea of how the string used to query
+	// this index should be formatted.
+	Type string
+	// Unique tells you whether there can be mutiple entries in the
+	// index for the same key that refer to different items.
+	Unique bool
+}
