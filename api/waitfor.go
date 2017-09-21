858479941a10915d27556eeba63f6e687e4964f3
Victor Lowther <victor.lowther@gmail.com>
2017-09-21 11:18:52 -0500

    Add comments to the rest of the public API components

--- api/waitfor.go
+++ api/waitfor.go
@@ -11,51 +11,52 @@ import (
 	"github.com/digitalrebar/provision/models"
 )
 
-func testItem(ref interface{}, field, value string) (bool, error) {
-	var err error
-	fields := map[string]interface{}{}
-	if err := utils.Remarshal(ref, fields); err != nil {
-		return false, err
-	}
-	matched := false
-
-	if d, ok := fields[field]; ok {
-		switch v := d.(type) {
-		case bool:
-			var bval bool
-			bval, err = strconv.ParseBool(value)
-			if err == nil {
-				if v == bval {
-					matched = true
-				}
-			}
-		case string:
-			if v == value {
-				matched = true
-			}
-		case int:
-			var ival int64
-			ival, err = strconv.ParseInt(value, 10, 64)
-			if err == nil {
-				if int(ival) == v {
-					matched = true
-				}
-			}
-		default:
-			err = fmt.Errorf("Unsupported field type: %T\n", d)
+func testItem(field, value string) func(interface{}) (bool, error) {
+	return func(ref interface{}) (bool, error) {
+		var err error
+		fields := map[string]interface{}{}
+		if err := utils.Remarshal(ref, fields); err != nil {
+			return false, err
 		}
+		matched := false
+		if d, ok := fields[field]; ok {
+			switch v := d.(type) {
+			case bool:
+				var bval bool
+				bval, err = strconv.ParseBool(value)
+				if err == nil {
+					if v == bval {
+						matched = true
+					}
+				}
+			case string:
+				if v == value {
+					matched = true
+				}
+			case int:
+				var ival int64
+				ival, err = strconv.ParseInt(value, 10, 64)
+				if err == nil {
+					if int(ival) == v {
+						matched = true
+					}
+				}
+			default:
+				err = fmt.Errorf("Unsupported field type: %T\n", d)
+			}
+		}
+		return matched, err
 	}
-	return matched, err
 }
 
-func (c *Client) WaitFor(item models.Model, field, value string, timeout int64) (string, error) {
-	found, err := testItem(item, field, value)
-	if err != nil {
-		return fmt.Sprintf("read: %v", err), err
-	}
-	if found {
-		return "complete", nil
-	}
+// WaitFor waits for an item to match test.  It subscribes to an
+// EventStream that watches all update and save envents for the object
+// in question, and returns a string indicating whether the match
+// succeeded, failed, or timed out.
+//
+// The API for this function is subject to refactoring and change, and
+// should not be considered to be stable yet.
+func (c *Client) WaitFor(item models.Model, test func(interface{}) (bool, error), timeout int64) (string, error) {
 	prefix := item.Prefix()
 	id := item.Key()
 	interrupt := make(chan os.Signal, 1)
@@ -66,7 +67,6 @@ func (c *Client) WaitFor(item models.Model, field, value string, timeout int64) 
 	if err != nil {
 		return "", err
 	}
-
 	if err := es.Register(evts...); err != nil {
 		es.Close()
 		return "", err
@@ -80,21 +80,21 @@ func (c *Client) WaitFor(item models.Model, field, value string, timeout int64) 
 		es.Close()
 	}()
 	for {
+		found, err := test(item)
+		if found && err == nil {
+			return "complete", nil
+		}
+		if err != nil {
+			return fmt.Sprintf("test: %v", err), err
+		}
 		select {
 		case evt := <-es.Events:
 			if evt.Err != nil {
 				return fmt.Sprintf("read: %v", err), err
 			}
-			obj, err := evt.E.Model()
+			item, err = evt.E.Model()
 			if err != nil {
 				return fmt.Sprintf("read: %v", err), err
-			}
-			found, err := testItem(obj, field, value)
-			if found && err == nil {
-				return "complete", nil
-			}
-			if err != nil {
-				return fmt.Sprintf("test: %v", err), err
 			}
 		case <-interrupt:
 			return "interrupt", nil
--- api/websocket.go
+++ api/websocket.go
@@ -22,15 +22,21 @@ func (c *Client) ws() (*websocket.Conn, error) {
 	return res, err
 }
 
+// RecievedEvent contains an event recieved from the digitalrebar
+// provision server along with any errors that occurred while
+// recieving the event.
 type RecievedEvent struct {
 	E   models.Event
 	Err error
 }
+
+// EventStream recieves events from the digitalrebar provider.  You can read recieved events by reading from its Events channel.
 type EventStream struct {
 	conn   *websocket.Conn
 	Events <-chan RecievedEvent
 }
 
+// Events creates a new EventStream from the client.
 func (c *Client) Events() (*EventStream, error) {
 	conn, err := c.ws()
 	if err != nil {
@@ -61,11 +67,28 @@ func (c *Client) Events() (*EventStream, error) {
 	return res, nil
 }
 
+// Close closes down the EventStream.  You should drain the Events
+// until you read a RecievedEvent that has an empty E and a non-nil
+// Err
 func (es *EventStream) Close() error {
 	return es.conn.WriteMessage(websocket.CloseMessage,
 		websocket.FormatCloseMessage(websocket.CloseNormalClosure, ""))
 }
 
+// Register directs the EventStream to subscribe to Events from the digital rebar provisioner.
+//
+// Event subscriptions consist of a string with the following format:
+//
+//    type.action.key
+//
+// type is the object type that you want to listen for events about.
+// * means to listen for events about all object types.
+//
+// action is the action that caused the event to be created.  * means
+// to listen for all actions.
+//
+// key is the unique identifier of the object to listen for.  * means
+// to listen for events from all objects
 func (es *EventStream) Register(events ...string) error {
 	for _, evt := range events {
 		if err := es.conn.WriteMessage(websocket.TextMessage, []byte("register "+evt)); err != nil {
@@ -75,6 +98,9 @@ func (es *EventStream) Register(events ...string) error {
 	return nil
 }
 
+// Deregister directs the EventStream to unsubscribe from Events from
+// the digitalrebar provisioner.  It takes the same parameters as
+// Register.
 func (es *EventStream) Deregister(events ...string) error {
 	for _, evt := range events {
 		if err := es.conn.WriteMessage(websocket.TextMessage, []byte("deregister "+evt)); err != nil {
