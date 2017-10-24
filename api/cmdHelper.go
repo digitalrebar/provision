60afd5435926c36535dc0da231c1d92ff005b7c7
Victor Lowther <victor.lowther@gmail.com>
2017-10-24 11:48:35 -0500

    Update jobs code in the API to include an agent implementation

---
+++ api/cmdHelper.go
@@ -0,0 +1,193 @@
+package api
+
+var cmdHelper = []byte(`
+#!/bin/bash
+
+# To force dpkg on Debian-based distros to play nice.
+export DEBIAN_FRONTEND=noninteractive DEBCONF_NONINTERACTIVE_SEEN=true
+
+# Force everything to use the C locale to keep things sane
+export LC_ALL=C LANGUAGE=C LANG=C
+
+# Make sure we play nice with debugging
+export PS4='${BASH_SOURCE}@${LINENO}(${FUNCNAME[0]}): '
+
+# Make sure the scripts are somewhat typo-resistant
+set -o pipefail -o errexit
+shopt -s nullglob extglob globstar
+
+# Make sure that $PATH is somewhat sane.
+fix_path() {
+    local -A pathparts
+    local part
+    local IFS=':'
+    for part in $PATH; do
+        pathparts["$part"]="true"
+    done
+    local wanted_pathparts=("/usr/local/bin" "/usr/local/sbin" "/bin" "/sbin" "/usr/bin" "/usr/sbin")
+    for part in "${wanted_pathparts[@]}"; do
+        [[ ${pathparts[$part]} ]] && continue
+        PATH="$part:$PATH"
+    done
+}
+fix_path
+unset fix_path
+
+# Figure out what Linux distro we are running on.
+export OS_TYPE= OS_VER= OS_NAME=
+if [[ -f /etc/os-release ]]; then
+    . /etc/os-release
+    OS_TYPE=${ID,,}
+    OS_VER=${VERSION_ID,,}
+elif [[ -f /etc/lsb-release ]]; then
+    . /etc/lsb-release
+    OS_VER=${DISTRIB_RELEASE,,}
+    OS_TYPE=${DISTRIB_ID,,}
+elif [[ -f /etc/centos-release || -f /etc/fedora-release || -f /etc/redhat-release ]]; then
+    for rel in centos-release fedora-release redhat-release; do
+        [[ -f /etc/$rel ]] || continue
+        OS_TYPE=${rel%%-*}
+        OS_VER="$(egrep -o '[0-9.]+' "/etc/$rel")"
+        break
+    done
+    if [[ ! $OS_TYPE ]]; then
+        echo "Cannot determine Linux version we are running on!"
+        exit 1
+    fi
+elif [[ -f /etc/debian_version ]]; then
+    OS_TYPE=debian
+    OS_VER=$(cat /etc/debian_version)
+fi
+OS_NAME="$OS_TYPE-$OS_VER"
+
+case $OS_TYPE in
+    centos|redhat|fedora|rhel|scientificlinux) OS_FAMILY="rhel";;
+    debian|ubuntu) OS_FAMILY="debian";;
+    *) OS_FAMILY=$OS_TYPE;;
+esac
+
+if_update_needed() {
+    local timestampref=/tmp/pkg_cache_update
+    if [[ ! -f $timestampref ]] || \
+           (( ($(stat -c '%Y' "$timestampref") - $(date '+%s')) > 86400 )); then
+        touch "$timestampref"
+        "$@"
+    fi
+}
+
+# Install a package
+install() {
+    local to_install=()
+    local pkg
+    for pkg in "$@"; do
+        to_install+=("$pkg")
+    done
+    case $OS_FAMILY in
+        rhel)
+            if_update_needed yum -y makecache
+            yum -y install "${to_install[@]}";;
+        debian)
+            if_update_needed apt-get -y update
+            apt-get -y install "${to_install[@]}";;
+        alpine)
+            if_update_needed apk update
+            apk add "${to_install[@]}";;
+        *) echo "No idea how to install packages on $OS_NAME"
+           exit 1;;
+    esac
+}
+
+INITSTYLE="sysv"
+if which systemctl &>/dev/null; then
+    INITSTYLE="systemd"
+elif which initctl &>/dev/null; then
+    INITSTYLE="upstart"
+fi
+
+# Perform service actions.
+service() {
+    # $1 = service name
+    # $2 = action to perform
+    local svc="$1"
+    shift
+    if which systemctl &>/dev/null; then
+        systemctl "$1" "$svc.service"
+    elif which chkconfig &>/dev/null; then
+        case $1 in
+            enable) chkconfig "$svc" on;;
+            disable) chkconfig "$svc" off;;
+            *)  command service "$svc" "$@";;
+        esac
+    elif which initctl &>/dev/null && initctl version 2>/dev/null | grep -q upstart ; then
+        /usr/sbin/service "$svc" "$1"
+    elif [[ -f /etc/init/$svc.unit ]]; then
+        initctl "$1" "$svc"
+    elif which update-rc.d &>/dev/null; then
+        case $1 in
+            enable|disable) update-rc.d "$svc" "$1";;
+            *) "/etc/init.d/$svc" "$1";;
+        esac
+    elif [[ -x /etc/init.d/$svc ]]; then
+        "/etc/init.d/$svc" "$1"
+    else
+        echo "No idea how to manage services on $OS_NAME"
+        exit 1
+    fi
+}
+
+get_param() {
+    # $1 attrib to get.  Attrib will be fetched in the context of the current machine
+    local attr
+    drpcli machines get "$RS_UUID" param "$1"
+}
+
+set_param() {
+    # $1 = name of the parameter to set
+    # $2 = parameter to set.
+    #      if $2 == "", then we will read from stdin
+    local src="$2"
+    if [[ ! $src ]]; then src="-"; fi
+    drpcli machines set "$RS_UUID" param "$1" to "$src"
+}
+
+__sane_exit() {
+    touch "$RS_TASK_DIR/.sane-exit-codes"
+}
+
+__exit() {
+    __sane_exit
+    exit $1
+}
+
+exit_incomplete() {
+    __exit 128
+}
+
+exit_reboot() {
+    __exit 64
+}
+
+exit_shutdown() {
+    __exit 32
+}
+
+exit_stop() {
+    __exit 16
+}
+
+exit_incomplete_reboot() {
+    __exit 192
+}
+
+exit_incomplete_shutdown() {
+    __exit 160
+}
+
+addr_port() {
+    if [[ $1 =~ ':' ]]; then
+        printf '[%s]:%d' "$1" "$2"
+    else
+        printf '%s:%d' "$1" "$2"
+    fi
+}
+`)
--- api/jobs.go
+++ api/jobs.go
@@ -6,53 +6,27 @@ import (
 	"bytes"
 	"fmt"
 	"io"
+	"io/ioutil"
+	"os"
+	"os/exec"
+	"path"
+	"strings"
+	"syscall"
+	"time"
 
+	"github.com/VictorLowther/jsonpatch2"
 	"github.com/digitalrebar/provision/models"
 )
 
-var exitOnFailure = false
-
-func (c *Client) AppendJobLog(j *models.Job, buf io.Reader) error {
-	uri := c.UrlFor("jobs", j.Key(), "log")
-	req, err := c.Request("PUT", uri, buf)
-	if err != nil {
-		return err
-	}
-	req.Header.Set("Content-Type", "application/octet-stream")
-	req.Header.Set("Accept", "application/json")
-	resp, err := c.Do(req)
-	if err != nil {
-		return err
-	}
-	defer resp.Body.Close()
-	return Unmarshal(resp, nil)
-}
-
-func (c *Client) JobLogString(j *models.Job, s string, items ...interface{}) error {
-	uri := c.UrlFor("jobs", j.Key(), "log")
-	buf := &bytes.Buffer{}
-	fmt.Fprintf(buf, s, items...)
-	req, err := c.Request("PUT", uri, buf)
-	if err != nil {
-		return err
-	}
-	req.Header.Set("Content-Type", "application/octet-stream")
-	req.Header.Set("Accept", "application/json")
-	resp, err := c.Do(req)
-	if err != nil {
-		return err
-	}
-	defer resp.Body.Close()
-	return Unmarshal(resp, nil)
-}
-
+// JobLog gets the log for a specific Job and writes it to the passed
+// io.Writer
 func (c *Client) JobLog(j *models.Job, dst io.Writer) error {
 	uri := c.UrlFor("jobs", j.Key(), "log")
 	req, err := c.Request("GET", uri, nil)
 	if err != nil {
 		return err
 	}
-	req.Header.Set("Accept", "application/octest-stream")
+	req.Header.Set("Accept", "application/octet-stream")
 	resp, err := c.Do(req)
 	if err != nil {
 		return err
@@ -61,18 +35,388 @@ func (c *Client) JobLog(j *models.Job, dst io.Writer) error {
 	return Unmarshal(resp, dst)
 }
 
+// JobActions returns the expanded list of templates that should be
+// written or executed for a specific Job.
 func (c *Client) JobActions(j *models.Job) ([]*models.JobAction, error) {
 	uri := c.UrlFor("jobs", j.Key(), "actions")
 	res := []*models.JobAction{}
 	return res, c.DoJSON("GET", uri, nil, &res)
 }
 
-/*
-
-func (c *Client) RunJobCommand(j *models.Job, action *models.JobAction) (failed, incomplete, reboot bool) {
-	logBody, logWriter := io.Pipe()
-	logger := bufio.NewWriter(logWriter)
-
-	c.JobLogString(j, "")
+// TaskRunner is responsible for expanding templates and running
+// scripts for a single task.
+type TaskRunner struct {
+	// Status codes that may be returned when a script exits.
+	failed, incomplete, reboot, poweroff, stop bool
+	// Client that the TaskRunner will use to communicate with the API
+	c *Client
+	// The Job that the TaskRunner will log to and update the status of.
+	j *models.Job
+	// The machine the TaskRunner is running on.
+	m *models.Machine
+	// The machine's current Task.
+	t *models.Task
+	// The io.Writer that all logging output goes to.
+	// It writes to stderr and to the Job on the server.
+	in io.Writer
+	// The write side of the pipe that communicates to the servver.
+	// Closing this will flush any data left in the pipe.
+	pipeWriter       *io.PipeWriter
+	agentDir, jobDir string
 }
-*/
+
+// NewTaskRunner creates a new TaskRunner for the passed-in machine.
+// It creates the matching Job (or resumes the previous incomplete
+// one), and handles making sure that all relavent output is written
+// to the job log as well as local stderr
+func NewTaskRunner(c *Client, m *models.Machine, agentDir string) (*TaskRunner, error) {
+	res := &TaskRunner{
+		c:        c,
+		m:        m,
+		agentDir: agentDir,
+	}
+	job := &models.Job{Machine: m.Uuid}
+	if err := c.CreateModel(job); err != nil {
+		return nil, err
+	}
+	if job.State == "" {
+		// Nothing to do.  Not an error
+		return nil, nil
+	}
+	if job.State != "created" && job.State != "incomplete" {
+		err := &models.Error{
+			Type:  "CLIENT_ERROR",
+			Model: job.Prefix(),
+			Key:   job.Key(),
+		}
+		err.Errorf("Invalid job state returned: %v", job.State)
+		return nil, err
+	}
+	t := &models.Task{Name: job.Task}
+	if err := c.FillModel(t, job.Task); err != nil {
+		return nil, err
+	}
+	res.j = job
+	res.t = t
+	return res, nil
+}
+
+// Close() shuts down the writer side of the logging pipe.
+// This will also flush any remaining data to stderr
+func (r *TaskRunner) Close() {
+	if r.pipeWriter != nil {
+		r.pipeWriter.Close()
+	}
+	os.Stderr.Sync()
+}
+
+// Log writes the string (with a timestamp) to stderr and to the
+// server-side log for the current job.
+func (r *TaskRunner) Log(s string, items ...interface{}) {
+	fmt.Fprintf(r.in, time.Now().String()+": "+s+"\n", items...)
+}
+
+// Expand a writes a file template to the appropriate location.
+func (r *TaskRunner) Expand(action *models.JobAction) error {
+	// Write the Contents of this template to the passed Path
+	if !strings.HasPrefix(action.Path, "/") {
+		action.Path = path.Join(r.agentDir, path.Clean(action.Path))
+	}
+	r.Log("%s: Writing %s to %s", time.Now(), action.Name, action.Path)
+	if err := ioutil.WriteFile(action.Path, []byte(action.Content), 0644); err != nil {
+		r.Log("Unable to write to %s: %v", action.Path, err)
+		return err
+	}
+	return nil
+}
+
+// Perform runs a single script action.
+func (r *TaskRunner) Perform(action *models.JobAction, taskDir string) error {
+	taskFile := path.Join(taskDir, r.j.Task+"-"+action.Name)
+	if err := ioutil.WriteFile(taskFile, []byte(action.Content), 0700); err != nil {
+		r.Log("Unable to write to script %s: %v", taskFile, err)
+		return err
+	}
+	cmd := exec.Command("./" + path.Base(taskFile))
+	cmd.Dir = taskDir
+	cmd.Env = append(os.Environ(), "RS_TASK_DIR="+taskDir, "RS_RUNNER_DIR="+r.agentDir)
+	cmd.Stdout = r.in
+	cmd.Stderr = r.in
+	r.Log("%s: Starting command %s\n\n", time.Now(), cmd.Path)
+	if err := cmd.Start(); err != nil {
+		r.Log("%s: Command failed to start")
+	}
+	// Wait on the process, not the command to exit.
+	// We don't want to auto-close stdout and stderr,
+	// as we will continue to use them.
+	pState, _ := cmd.Process.Wait()
+	status := pState.Sys().(syscall.WaitStatus)
+	sane := r.t.HasFeature("sane-exit-codes")
+	if !sane {
+		st, err := os.Stat(path.Join(taskDir, ".sane-exit-codes"))
+		sane = err == nil && st.Mode().IsRegular()
+	}
+	code := uint(status.ExitStatus())
+	if sane {
+		// codes can be between 0 and 255
+		// if the low bits are not 0, the command failed.
+		r.failed = code&^240 > uint(0)
+		// If the high bit is set, the command was incomplete and the
+		// the current task pointer should not be advanced.
+		r.incomplete = code&128 > uint(0)
+		// If we need a reboot, set bit 6
+		r.reboot = code&64 > uint(0)
+		// If we need to poweroff, set bit 5.  Reboot wins if it is set.
+		r.poweroff = code&32 > uint(0)
+		// If we need to stop, set bit 4
+		r.stop = code&16 > uint(0)
+	} else {
+		switch code {
+		case 0:
+		case 1:
+			r.reboot = true
+		case 2:
+			r.incomplete = true
+		case 3:
+			r.incomplete = true
+			r.reboot = true
+		default:
+			r.failed = true
+		}
+	}
+	return nil
+}
+
+// Run loops over all of the actions for a particular job,
+// placing files and executing scripts as appropriate.
+// It also arranges for all logging output for the actions
+// to go to the right places.
+func (r *TaskRunner) Run() error {
+	finalErr := &models.Error{
+		Type:  "RUNNER_ERR",
+		Model: r.j.Prefix(),
+		Key:   r.j.Key(),
+	}
+	// Arrange to log everything to the job log and stderr at the same time.
+	// Due to how io.Pipe works, this should wind up being fairly synchronous.
+	reader, writer := io.Pipe()
+	r.in = io.MultiWriter(writer, os.Stderr)
+	r.pipeWriter = writer
+	helperWritten := false
+	go func() {
+		defer reader.Close()
+		uri := r.c.UrlFor("jobs", r.j.Key(), "log")
+		buf := bytes.NewBuffer(make([]byte, 64*1024))
+		for {
+			count, err := io.CopyN(buf, reader, 64*1024)
+			if count > 0 {
+				req, err := r.c.Request("PUT", uri, buf)
+				if err != nil {
+					return
+				}
+				req.Header.Set("Content-Type", "application/octet-stream")
+				req.Header.Set("Accept", "application/json")
+				resp, err := r.c.Do(req)
+				if err != nil {
+					return
+				}
+				resp.Body.Close()
+				buf.Reset()
+			}
+			if err != nil {
+				return
+			}
+		}
+	}()
+	// We are responsible for going from created to running.
+	// If this patch fails, we cannot do it
+	patch := jsonpatch2.Patch{
+		{Op: "test", Path: "/State", Value: "created"},
+		{Op: "replace", Path: "/State", Value: "running"},
+	}
+	finalState := "failed"
+	taskDir, err := ioutil.TempDir(r.agentDir, r.j.Task+"-")
+	if err != nil {
+		r.Log("Failed to create local tmpdir: %v", err)
+		finalErr.AddError(err)
+		return finalErr
+	}
+	// No matter how the function exits, we will try to patch the Job
+	// to an appropriate final state.
+	defer os.RemoveAll(taskDir)
+	defer func() {
+		finalPatch := jsonpatch2.Patch{
+			{Op: "test", Path: "/State", Value: "running"},
+			{Op: "replace", Path: "/State", Value: finalState},
+		}
+		obj, err := r.c.PatchModel(r.j.Prefix(), r.j.Key(), finalPatch)
+		if err != nil {
+			r.j = obj.(*models.Job)
+		}
+	}()
+	obj, err := r.c.PatchModel(r.j.Prefix(), r.j.Key(), patch)
+	if err != nil {
+		finalErr.AddError(err)
+		return finalErr
+	}
+	r.j = obj.(*models.Job)
+	r.Log("Starting task %s on %s", r.j.Task, r.m.Uuid)
+	// At this point, we are running.
+	actions, err := r.c.JobActions(r.j)
+	if err != nil {
+		r.Log("Failed to render actions: %v", err)
+		finalErr.AddError(err)
+		return finalErr
+	}
+	for _, action := range actions {
+		r.failed = false
+		r.incomplete = false
+		r.poweroff = false
+		r.reboot = false
+		r.stop = false
+		var err error
+		if action.Path != "" {
+			err = r.Expand(action)
+		} else {
+			if !helperWritten {
+				err := ioutil.WriteFile(path.Join(taskDir, "helper"), cmdHelper, 0600)
+				if err != nil {
+					finalErr.AddError(err)
+					return finalErr
+				}
+				helperWritten = true
+			}
+			err = r.Perform(action, taskDir)
+			// Contents is a script to run, run it.
+		}
+		if err != nil {
+			finalErr.AddError(err)
+			return finalErr
+		}
+		if r.failed {
+			finalState = "failed"
+		} else if r.incomplete {
+			finalState = "incomplete"
+		}
+		if r.failed || r.incomplete || r.reboot || r.poweroff || r.stop {
+			break
+		}
+	}
+	if finalState == "running" {
+		finalState = "finished"
+	}
+	return nil
+}
+
+// Agent runs the machine Agent on the current machine.
+// It assumes there is only one Agent, which is not actually a safe assumption.
+// We should make it safe someday.
+func (c *Client) Agent(m *models.Machine, exitOnFailure, actuallyPowerThings bool) error {
+	// Clear the current running job, if any
+	currentJob := &models.Job{}
+	if c.FillModel(currentJob, m.CurrentJob.String()) == nil {
+		if currentJob.State == "running" || currentJob.State == "created" {
+			currentJob.State = "failed"
+			if err := c.PutModel(currentJob); err != nil {
+				return err
+			}
+		}
+	}
+	runnerDir, err := ioutil.TempDir("", "runner-")
+	if err != nil {
+		return err
+	}
+	defer os.RemoveAll(runnerDir)
+	events, err := c.Events()
+	if err != nil {
+		return err
+	}
+	defer events.Close()
+	newM := models.Clone(m).(*models.Machine)
+	newM.Runnable = true
+	obj, err := c.PatchTo(m, newM)
+	if err != nil {
+		return err
+	}
+	m = obj.(*models.Machine)
+	var runner *TaskRunner
+	for {
+		if runner != nil {
+			runner.Close()
+			runner = nil
+		}
+		found, err := events.WaitFor(m, TestItem("Runnable", "true"), 1*time.Hour)
+		if err != nil {
+			res := &models.Error{
+				Type:  "AGENT_WAIT",
+				Model: m.Prefix(),
+				Key:   m.Key(),
+			}
+			res.Errorf("Event wait failed: %s", found)
+			res.AddError(err)
+			return res
+		}
+		switch found {
+		case "timeout":
+			continue
+		case "interrupt":
+			break
+		case "complete":
+		default:
+			res := &models.Error{
+				Type:  "AGENT_WAIT",
+				Model: m.Prefix(),
+				Key:   m.Key(),
+			}
+			res.Errorf("Unexpected return from WaitFor: %s", found)
+			return res
+		}
+		runner, err = NewTaskRunner(c, m, runnerDir)
+		if err != nil {
+			return err
+		}
+		if err := runner.Run(); err != nil {
+			return err
+		}
+		if runner.reboot ||
+			runner.poweroff ||
+			runner.stop ||
+			runner.incomplete ||
+			(exitOnFailure && runner.failed) {
+			break
+		}
+	}
+	if runner == nil {
+		return nil
+	}
+	defer runner.Close()
+	if runner.reboot {
+		if actuallyPowerThings {
+			_, err := exec.Command("reboot").Output()
+			if err != nil {
+				runner.Log("Failed to issue reboot: %v", err)
+			}
+		} else {
+			runner.Log("Would have rebooted")
+		}
+	}
+	if runner.poweroff {
+		if actuallyPowerThings {
+			_, err := exec.Command("poweroff").Output()
+			if err != nil {
+				runner.Log("Failed to issue poweroff: %v", err)
+			}
+		} else {
+			runner.Log("Would have powered down")
+		}
+	}
+	// If we failed, should we exit
+	if runner.failed {
+		runner.Log("Task failed")
+	}
+	// Task asked to stop
+	if runner.stop {
+		runner.Log("Task signalled runner to stop")
+	}
+	return nil
+}
