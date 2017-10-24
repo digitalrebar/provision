b76bdb3ee3df2ad0c7a8f31174a1eb8fe9d1de1a
Victor Lowther <victor.lowther@gmail.com>
2017-10-24 11:23:34 -0500

    Consolidate eventstream code into its own file in the API

--- api/event_stream.go
+++ api/event_stream.go
@@ -8,14 +8,56 @@ import (
 	"os"
 	"os/signal"
 	"sort"
+	"strconv"
 	"strings"
 	"sync"
 	"sync/atomic"
 	"time"
 
+	"github.com/VictorLowther/jsonpatch2/utils"
 	"github.com/digitalrebar/provision/models"
 	"github.com/gorilla/websocket"
 )
+
+// TestItem creates a test function to see if a value in the
+// passed interface is true.
+func TestItem(field, value string) func(interface{}) (bool, error) {
+	return func(ref interface{}) (bool, error) {
+		var err error
+		fields := map[string]interface{}{}
+		if err := utils.Remarshal(ref, fields); err != nil {
+			return false, err
+		}
+		matched := false
+		if d, ok := fields[field]; ok {
+			switch v := d.(type) {
+			case bool:
+				var bval bool
+				bval, err = strconv.ParseBool(value)
+				if err == nil {
+					if v == bval {
+						matched = true
+					}
+				}
+			case string:
+				if v == value {
+					matched = true
+				}
+			case int:
+				var ival int64
+				ival, err = strconv.ParseInt(value, 10, 64)
+				if err == nil {
+					if int(ival) == v {
+						matched = true
+					}
+				}
+			default:
+				err = fmt.Errorf("Unsupported field type: %T\n", d)
+			}
+		}
+		return matched, err
+	}
+}
 
 func (c *Client) ws() (*websocket.Conn, error) {
 	ep := c.UrlFor("ws")
@@ -213,7 +255,10 @@ func (es *EventStream) Deregister(handle int64) error {
 //
 // The API for this function is subject to refactoring and change, and
 // should not be considered to be stable yet.
-func (es *EventStream) WaitFor(item models.Model, test func(interface{}) (bool, error), timeout int64) (string, error) {
+func (es *EventStream) WaitFor(
+	item models.Model,
+	test func(interface{}) (bool, error),
+	timeout time.Duration) (string, error) {
 	prefix := item.Prefix()
 	id := item.Key()
 	interrupt := make(chan os.Signal, 1)
