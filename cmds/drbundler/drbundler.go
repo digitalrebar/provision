c154a31d413eadebd4af8dca7c973af4a3b9fc47
Greg Althaus <galthaus@austin.rr.com>
2017-10-25 15:21:28 -0500

    have drbundler output different formats based upon the filename ext (.go, .yaml/.yml, or .json)

--- cmds/drbundler/drbundler.go
+++ cmds/drbundler/drbundler.go
@@ -4,54 +4,76 @@ import (
 	"fmt"
 	"io/ioutil"
 	"os"
+	"path"
 
 	"github.com/digitalrebar/provision/api"
 	"github.com/digitalrebar/store"
 )
 
+func outputBuffer(filename string, buf []byte) error {
+	ext := path.Ext(filename)
+	if ext == ".go" {
+		s := fmt.Sprintf("package main\n\nvar contentYamlString = `%s`\n", string(buf))
+		buf = []byte(s)
+	}
+
+	if err := ioutil.WriteFile(filename, buf, 0644); err != nil {
+		return err
+	}
+	return nil
+
+}
+
+func cleanUp(filename, output string) {
+	os.Remove(filename + ".tmp")
+	fmt.Printf(output)
+	os.Exit(1)
+}
+
 func main() {
 	args := os.Args
 
 	if len(args) != 3 {
-		fmt.Printf("Must provide a directory and a file\n")
-		os.Exit(1)
+		cleanUp("", "Must provide a directory and a file\n")
 	}
 
 	directory := args[1]
 	filename := args[2]
 
 	if _, err := os.Stat(directory); os.IsNotExist(err) {
-		s := fmt.Sprintf("package main\n\nvar contentYamlString = \"\"\n")
-		buf := []byte(s)
-		if err := ioutil.WriteFile(filename, buf, 0644); err != nil {
-			fmt.Printf("Failed to write file: %v\n", err)
-			os.Exit(1)
+		if err := outputBuffer(filename, []byte("")); err != nil {
+			cleanUp(filename, fmt.Sprintf("Failed to write file: %v\n", err))
 		}
 		os.Exit(0)
 	}
 
-	if dst, err := store.Open(fmt.Sprintf("file:%s.tmp?codec=yaml", filename)); err != nil {
-		fmt.Printf("Failed to open store: %v\n", err)
-		os.Exit(1)
+	ext := path.Ext(filename)
+	codec := "yaml"
+	if ext == ".go" {
+		codec = "yaml"
+	} else if ext == ".yaml" || ext == ".yml" {
+		codec = "yaml"
+	} else if ext == ".json" {
+		codec = "json"
+	} else {
+		cleanUp(filename, fmt.Sprintf("Unknown extension: %s\n", ext))
+	}
 
+	if dst, err := store.Open(fmt.Sprintf("file:%s.tmp?codec=%s", filename, codec)); err != nil {
+		cleanUp(filename, fmt.Sprintf("Failed to open store: %v\n", err))
 	} else {
 		client := &api.Client{}
 		if err := client.BundleContent(directory, dst, map[string]string{}); err != nil {
-			fmt.Printf("Failed to load: %v\n", err)
-			os.Exit(1)
+			cleanUp(filename, fmt.Sprintf("Failed to load: %v\n", err))
 		}
 		dst.Close()
 	}
 
 	if contents, err := ioutil.ReadFile(filename + ".tmp"); err != nil {
-		fmt.Printf("Failed to readfile: %v\n", err)
-		os.Exit(1)
+		cleanUp(filename, fmt.Sprintf("Failed to readfile: %v\n", err))
 	} else {
-		s := fmt.Sprintf("package main\n\nvar contentYamlString = `\n%s\n`\n", string(contents))
-		buf := []byte(s)
-		if err := ioutil.WriteFile(filename, buf, 0644); err != nil {
-			fmt.Printf("Failed to write file: %v\n", err)
-			os.Exit(1)
+		if err := outputBuffer(filename, contents); err != nil {
+			cleanUp(filename, fmt.Sprintf("Failed to write file: %v\n", err))
 		}
 	}
 	os.Remove(filename + ".tmp")
