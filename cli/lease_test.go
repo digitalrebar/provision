a0b2a9e8e570a0e2b1880fa683e6993f96f41431
Greg Althaus <galthaus@austin.rr.com>
2018-02-02 10:20:35 -0600

    Add Lease filter tests and clean up some dead code.

--- cli/lease_test.go
+++ cli/lease_test.go
@@ -7,4 +7,13 @@ import (
 func TestLeaseCli(t *testing.T) {
 	cliTest(true, false, "leases").run(t)
 	cliTest(false, false, "leases", "list").run(t)
+	cliTest(false, true, "leases", "list", "Addr=fred").run(t)
+	cliTest(false, false, "leases", "list", "Addr=1.1.1.1").run(t)
+	cliTest(false, false, "leases", "list", "Token=1.1.1.1").run(t)
+	cliTest(false, false, "leases", "list", "Token=11:22:33:44:55:66").run(t)
+	cliTest(false, false, "leases", "list", "Strategy=MAC").run(t)
+	cliTest(false, false, "leases", "list", "Strategy=COW").run(t)
+	cliTest(false, false, "leases", "list", "State=sleep").run(t)
+	cliTest(false, true, "leases", "list", "ExpireTime=fred").run(t)
+	cliTest(false, false, "leases", "list", "ExpireTime=2006-01-02T15:04:05-07:00").run(t)
 }
