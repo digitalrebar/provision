25cdf0e0b2bebe4890096fb0c6d97441112fe977
Greg Althaus <galthaus@austin.rr.com>
2018-03-05 11:04:26 -0600

    Add windows cli into DRP

--- api/files_test.go
+++ api/files_test.go
@@ -12,7 +12,7 @@ func TestFiles(t *testing.T) {
 	tests := []crudTest{
 		{
 			name:      "list files",
-			expectRes: []string{"drpcli.amd64.linux", "jq", "plugin_providers/"},
+			expectRes: []string{"drpcli.amd64.linux", "drpcli.amd64.windows", "jq", "plugin_providers/"},
 			expectErr: nil,
 			op: func() (interface{}, error) {
 				return session.ListBlobs("files")
@@ -72,6 +72,7 @@ func TestFiles(t *testing.T) {
 			expectRes: []string{
 				"bar/",
 				"drpcli.amd64.linux",
+				"drpcli.amd64.windows",
 				"foo",
 				"jq",
 				"plugin_providers/",
