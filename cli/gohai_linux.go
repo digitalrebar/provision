b001303783fbad63b2b97fb9abed7ad08e3a227b
Victor Lowther <victor.lowther@gmail.com>
2019-04-01 13:11:35 -0500

    Do not error out immediately when we fail to gather a class of inventory data

--- cli/gohai_linux.go
+++ cli/gohai_linux.go
@@ -3,8 +3,7 @@
 package cli
 
 import (
-	"fmt"
-
+	"github.com/digitalrebar/provision/models"
 	"github.com/rackn/gohai/plugins/dmi"
 	"github.com/rackn/gohai/plugins/net"
 	"github.com/rackn/gohai/plugins/storage"
@@ -16,27 +15,28 @@ type dmiinfo interface {
 }
 
 func gohai() error {
+	res := &models.Error{}
 	infos := map[string]dmiinfo{}
 	defer prettyPrint(infos)
 	dmiInfo, err := dmi.Gather()
 	if err != nil {
-		return fmt.Errorf("Failed to gather DMI information: %v", err)
+		res.Errorf("Failed to gather DMI information: %v", err)
 	}
 	infos[dmiInfo.Class()] = dmiInfo
 	netInfo, err := net.Gather()
 	if err != nil {
-		return fmt.Errorf("Failed to gather network info: %v", err)
+		res.Errorf("Failed to gather network info: %v", err)
 	}
 	infos[netInfo.Class()] = netInfo
 	sysInfo, err := system.Gather()
 	if err != nil {
-		return fmt.Errorf("Failed to gather basic OS info: %v", err)
+		res.Errorf("Failed to gather basic OS info: %v", err)
 	}
 	infos[sysInfo.Class()] = sysInfo
 	storInfo, err := storage.Gather()
 	if err != nil {
-		return fmt.Errorf("Failed tp gather storage info: %v", err)
+		res.Errorf("Failed to gather storage info: %v", err)
 	}
 	infos[storInfo.Class()] = storInfo
-	return nil
+	return res.HasError()
 }
