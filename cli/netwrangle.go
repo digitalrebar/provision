648876004002886bcadcdb1629ba67ecd4f44b1d
Victor Lowther <victor.lowther@gmail.com>
2019-06-27 14:58:20 -0500

    Add drpcli net autogen [machine-id]
    This new command  figures out the local network config  file format in
    use for several common Linux distros,  and writes out a network config
    that can drive it.
    
    It is intended to be used during the OS install path as a post-install
    task.

--- cli/netwrangle.go
+++ cli/netwrangle.go
@@ -4,6 +4,12 @@ package cli
 
 import (
 	"fmt"
+	"io/ioutil"
+	"os"
+	"os/exec"
+	"path"
+	"path/filepath"
+	"strings"
 
 	"github.com/rackn/netwrangler"
 	"github.com/rackn/netwrangler/netplan"
@@ -13,6 +19,137 @@ import (
 
 func init() {
 	addRegistrar(registerNet)
+}
+
+func writeNetCfg(machine, format, dest string, bindMac bool) error {
+	topology := &netplan.Netplan{}
+	addressing := map[string]interface{}{}
+	bootMac := ""
+	if err := session.Req().
+		UrlFor("machines", machine, "params", "net/interface-topology").
+		Params("aggregate", "true").Do(topology); err != nil {
+		return err
+	}
+	if err := session.Req().
+		UrlFor("machines", machine, "params", "net/interface-config").
+		Params("aggregate", "true").Do(&addressing); err != nil {
+		return err
+	}
+	if err := session.Req().
+		UrlFor("machines", machine, "params", "last-boot-macaddr").
+		Do(&bootMac); err != nil {
+		return err
+	}
+	netwrangler.BootMac(bootMac)
+	phys, err := netwrangler.GatherPhys()
+	if err != nil {
+		return err
+	}
+	if topology.Network.Vlans != nil {
+		for k := range topology.Network.Vlans {
+			v, ok := addressing[k]
+			if !ok {
+				continue
+			}
+			delete(addressing, k)
+			from, fromOk := v.(map[string]interface{})
+			to, toOk := topology.Network.Vlans[k].(map[string]interface{})
+			if !fromOk || !toOk {
+				continue
+			}
+			for fk := range from {
+				to[fk] = from[fk]
+			}
+			topology.Network.Vlans[k] = to
+		}
+	}
+	if topology.Network.Bonds != nil {
+		for k := range topology.Network.Bonds {
+			v, ok := addressing[k]
+			if !ok {
+				continue
+			}
+			delete(addressing, k)
+			from, fromOk := v.(map[string]interface{})
+			to, toOk := topology.Network.Bonds[k].(map[string]interface{})
+			if !fromOk || !toOk {
+				continue
+			}
+			for fk := range from {
+				to[fk] = from[fk]
+			}
+			topology.Network.Bonds[k] = to
+		}
+	}
+	if topology.Network.Bridges != nil {
+		for k := range topology.Network.Bridges {
+			v, ok := addressing[k]
+			if !ok {
+				continue
+			}
+			delete(addressing, k)
+			from, fromOk := v.(map[string]interface{})
+			to, toOk := topology.Network.Bridges[k].(map[string]interface{})
+			if !fromOk || !toOk {
+				continue
+			}
+			for fk := range from {
+				to[fk] = from[fk]
+			}
+			topology.Network.Bridges[k] = to
+		}
+	}
+	if topology.Network.Ethernets == nil {
+		topology.Network.Ethernets = map[string]interface{}{}
+	}
+	for k := range topology.Network.Ethernets {
+		v, ok := addressing[k]
+		if !ok {
+			continue
+		}
+		delete(addressing, k)
+		from, fromOk := v.(map[string]interface{})
+		to, toOk := topology.Network.Ethernets[k].(map[string]interface{})
+		if !fromOk || !toOk {
+			continue
+		}
+		for fk := range from {
+			to[fk] = from[fk]
+		}
+		topology.Network.Ethernets[k] = to
+	}
+	for k := range addressing {
+		topology.Network.Ethernets[k] = addressing[k]
+	}
+	layout, err := topology.Compile(phys)
+	if err != nil {
+		return err
+	}
+	return netwrangler.Write(layout, format, dest, bindMac)
+}
+
+func cleanTarget(tgtDir, glob, exclude string) error {
+	g, err := filepath.Glob(path.Join(tgtDir, glob))
+	if err != nil {
+		return err
+	}
+	for _, tgt := range g {
+		if strings.HasSuffix(tgt, "/..") ||
+			strings.HasSuffix(tgt, "/.") {
+			continue
+		}
+		if m, err := filepath.Match(path.Join(tgtDir, exclude), tgt); err == nil && m {
+			continue
+		}
+		os.RemoveAll(tgt)
+	}
+	return nil
+}
+
+func copyTarget(src, dest string) error {
+	ee := &util.Err{}
+	util.Copy(src, dest, ee)
+	return ee.OrNil()
 }
 
 func registerNet(app *cobra.Command) {
@@ -90,112 +227,78 @@ func registerNet(app *cobra.Command) {
 			return nil
 		},
 		RunE: func(c *cobra.Command, args []string) error {
-			topology := &netplan.Netplan{}
-			addressing := map[string]interface{}{}
-			if err := session.Req().
-				UrlFor("machines", args[2], "params", "net/interface-topology").
-				Params("aggregate", "true").Do(topology); err != nil {
-				return err
-			}
-			if err := session.Req().
-				UrlFor("machines", args[2], "params", "net/interface-config").
-				Params("aggregate", "true").Do(&addressing); err != nil {
-				return err
-			}
-			if err := session.Req().
-				UrlFor("machines", args[2], "params", "last-boot-macaddr").
-				Do(&bootMac); err != nil {
-				return err
-			}
-			netwrangler.BootMac(bootMac)
-			phys, err := netwrangler.GatherPhys()
-			if err != nil {
-				return err
-			}
-			if topology.Network.Vlans != nil {
-				for k := range topology.Network.Vlans {
-					v, ok := addressing[k]
-					if !ok {
-						continue
-					}
-					delete(addressing, k)
-					from, fromOk := v.(map[string]interface{})
-					to, toOk := topology.Network.Vlans[k].(map[string]interface{})
-					if !fromOk || !toOk {
-						continue
-					}
-					for fk := range from {
-						to[fk] = from[fk]
-					}
-					topology.Network.Vlans[k] = to
-				}
-			}
-			if topology.Network.Bonds != nil {
-				for k := range topology.Network.Bonds {
-					v, ok := addressing[k]
-					if !ok {
-						continue
-					}
-					delete(addressing, k)
-					from, fromOk := v.(map[string]interface{})
-					to, toOk := topology.Network.Bonds[k].(map[string]interface{})
-					if !fromOk || !toOk {
-						continue
-					}
-					for fk := range from {
-						to[fk] = from[fk]
-					}
-					topology.Network.Bonds[k] = to
-				}
-			}
-			if topology.Network.Bridges != nil {
-				for k := range topology.Network.Bridges {
-					v, ok := addressing[k]
-					if !ok {
-						continue
-					}
-					delete(addressing, k)
-					from, fromOk := v.(map[string]interface{})
-					to, toOk := topology.Network.Bridges[k].(map[string]interface{})
-					if !fromOk || !toOk {
-						continue
-					}
-					for fk := range from {
-						to[fk] = from[fk]
-					}
-					topology.Network.Bridges[k] = to
-				}
-			}
-			if topology.Network.Ethernets == nil {
-				topology.Network.Ethernets = map[string]interface{}{}
-			}
-			for k := range topology.Network.Ethernets {
-				v, ok := addressing[k]
-				if !ok {
-					continue
-				}
-				delete(addressing, k)
-				from, fromOk := v.(map[string]interface{})
-				to, toOk := topology.Network.Ethernets[k].(map[string]interface{})
-				if !fromOk || !toOk {
-					continue
-				}
-				for fk := range from {
-					to[fk] = from[fk]
-				}
-				topology.Network.Ethernets[k] = to
-			}
-			for k := range addressing {
-				topology.Network.Ethernets[k] = addressing[k]
-			}
-			layout, err := topology.Compile(phys)
-			if err != nil {
-				return err
-			}
-			return netwrangler.Write(layout, args[0], args[4], bindMac)
+			return writeNetCfg(args[2], args[0], args[4], bindMac)
 		},
 	}
 	generate.Flags().BoolVar(&bindMac, "bindMac", true, "Bind all base nic definitions to their mac address. Defaults to true, otherwise name bindings will be used.")
 	net.AddCommand(generate)
+	autogen := &cobra.Command{
+		Use:   "autogen [machine-id]",
+		Short: "Autogen network configuration for [machine-id] based on the network configuration mechanism it currently uses",
+		Long: fmt.Sprintf(`[format] must be one of: %v
+[machine-id] must me the UUID for the machine in question
+[dest] must be the final location for the network configuration in [format]`, netwrangler.DestFormats),
+		Args: func(c *cobra.Command, args []string) error {
+			if len(args) != 1 {
+				return fmt.Errorf("%v requires 1 argument", c.UseLine())
+			}
+			return nil
+		},
+		RunE: func(c *cobra.Command, args []string) error {
+			machine := args[0]
+			var format, target string
+			if fi, err := os.Stat("/etc/netplan"); err == nil && fi.IsDir() {
+				format, target = "netplan", "/etc/netplan"
+			} else if fi, err := os.Stat("/etc/systemd/network"); err == nil && fi.IsDir() {
+				format, target = "systemd", "/etc/systemd/network"
+			} else if fi, err := os.Stat("/etc/sysconfig/network-scripts"); err == nil && fi.IsDir() {
+				format, target = "rhel", "/etc/sysconfig/network-scripts"
+			} else if fi, err := os.Stat("/etc/network/interfaces"); err == nil && fi.Mode().IsRegular() {
+				format, target = "debian", "/etc/network"
+			} else {
+				return fmt.Errorf("Unable to determine how to configure the network")
+			}
+			temp, err := ioutil.TempDir("", "drpcli-autogen")
+			if err != nil {
+				return err
+			}
+			defer os.RemoveAll(temp)
+			switch format {
+			case "netplan":
+				if err := writeNetCfg(machine, "netplan", path.Join(temp, "01-dr-provision-auto.yaml"), bindMac); err != nil {
+					return err
+				}
+				if err := cleanTarget(target, "*", ""); err != nil {
+					return err
+				}
+				if err := copyTarget(temp, target); err != nil {
+					return err
+				}
+				cmd := exec.Command("netplan", "generate")
+				cmd.Stderr = os.Stderr
+				cmd.Stdout = os.Stdout
+				return cmd.Run()
+			case "systemd":
+				if err := writeNetCfg(machine, "systemd", temp, bindMac); err != nil {
+					return err
+				}
+				if err := cleanTarget(target, "*", ""); err != nil {
+					return err
+				}
+				return copyTarget(temp, target)
+			case "rhel":
+				if err := writeNetCfg(machine, "rhel", temp, bindMac); err != nil {
+					return err
+				}
+				if err := cleanTarget(target, "ifcfg-*", "ifcfg-lo*"); err != nil {
+					return err
+				}
+				return copyTarget(temp, target)
+			}
+			return fmt.Errorf("Cannot configure network information for %s", format)
+		},
+	}
+	autogen.Flags().BoolVar(&bindMac, "bindMac", true, "Bind all base nic definitions to their mac address. Defaults to true, otherwise name bindings will be used.")
+	net.AddCommand(autogen)
 	app.AddCommand(net)
 }
