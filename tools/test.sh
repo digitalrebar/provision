194fd51fd47ad34c877be322162e73c85df66b77
Victor Lowther <victor.lowther@gmail.com>
2019-07-30 11:41:16 -0500

    Add basic testing to see if dr-provision is around and a sane version.

--- tools/test.sh
+++ tools/test.sh
@@ -1,6 +1,18 @@
 #!/usr/bin/env bash
 PATH="$PWD/bin/$(go env GOOS)/$(go env GOARCH):$PATH"
 
+if ! which dr-provision &>/dev/null; then
+    echo "No dr-provision binary to run tests against"
+    exit 1
+fi
+ver_re='v4\.[0-9]+\.[0-9]+.*'
+if ! [[ $(dr-provision --version 2>&1) =~ $ver_re ]]; then
+    echo "Make sure a dr-provision binary of at least v4.0.0 or later is in your PATH"
+    exit 1
+fi
+
+echo Running with $(which dr-provision) version $BASH_REMATCH
+
 packages="github.com/digitalrebar/provision/v4,\
 github.com/digitalrebar/provision/v4/models,\
 github.com/digitalrebar/provision/v4/plugin,\
@@ -15,3 +27,5 @@ for d in $(go list ./... 2>/dev/null | egrep -v 'cmds|test') ; do
     time go test -timeout 30m -race -covermode=atomic -coverpkg=$packages -coverprofile="profile${i}.txt" "$d" || FAILED=true
     i=$((i+1))
 done
+
+[[ ! $FAILED ]]
