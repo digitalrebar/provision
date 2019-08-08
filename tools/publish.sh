f2a4113024e123c4bc4332c80ae8da4fb4376067
Greg Althaus <galthaus@austin.rr.com>
2019-08-08 16:13:25 -0500

    Fix publish of windows drpcli - update gitignore

--- tools/publish.sh
+++ tools/publish.sh
@@ -19,7 +19,7 @@ cp bin/linux/amd64/drpcli rebar-catalog/drpcli/amd64/linux
 mkdir -p rebar-catalog/drpcli/amd64/darwin
 cp bin/darwin/amd64/drpcli rebar-catalog/drpcli/amd64/darwin
 mkdir -p rebar-catalog/drpcli/amd64/windows
-cp bin/windows/amd64/drpcli rebar-catalog/drpcli/amd64/windows
+cp bin/windows/amd64/drpcli.exe rebar-catalog/drpcli/amd64/windows
 mkdir -p rebar-catalog/drpcli/arm64/linux
 cp bin/linux/arm64/drpcli rebar-catalog/drpcli/arm64/linux
 mkdir -p rebar-catalog/drpcli/armv7/linux
