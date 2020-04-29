8c7a794ef2b91ba0e2794eb580937e2231ae14f6
Victor Lowther <victor.lowther@gmail.com>
2020-04-29 11:16:59 -0500

    Add comments to whoami and add a whoami machine action

--- models/role.go
+++ models/role.go
@@ -37,7 +37,7 @@ var (
 	addedActions = map[string]string{
 		"users":     "token, password",
 		"jobs":      "log",
-		"machines":  "getSecure, updateSecure, updateTaskList, token",
+		"machines":  "getSecure, updateSecure, updateTaskList, token, whoami",
 		"plugins":   "getSecure, updateSecure",
 		"profiles":  "getSecure, updateSecure",
 		"stages":    "getSecure, updateSecure",
--- models/whoami.go
+++ models/whoami.go
@@ -13,6 +13,8 @@ import (
 	"github.com/pborman/uuid"
 )
 
+// Whoami contains the elements used toi fingerprint a machine, along with
+// the results of the fingerprint comparison request
 type Whoami struct {
 	Result struct {
 		Score int       `json:",omitempty"`
@@ -24,6 +26,8 @@ type Whoami struct {
 	OnDiskUUID  string
 }
 
+// Fill fills out the MacAddrs list and the MachineFingerprint from
+// the DMI information on the machine.
 func (w *Whoami) Fill() error {
 	(&w.Fingerprint).Fill()
 	w.MacAddrs = []string{}
@@ -68,16 +72,45 @@ func (w *Whoami) Fill() error {
 	return nil
 }
 
+// FromMachine extracts the Fingerprint and HardwareAddrs fields from a Machine
+// and populates Whoami with it.
 func (w *Whoami) FromMachine(m *Machine) {
 	w.MacAddrs = append([]string{}, m.HardwareAddrs...)
 	w.Fingerprint = m.Fingerprint
 }
 
+// ToMachine saves the Fingerprint and the MacAddrs fields onto the passed Machine.
 func (w *Whoami) ToMachine(m *Machine) {
 	m.HardwareAddrs = append([]string{}, w.MacAddrs...)
 	m.Fingerprint = w.Fingerprint
 }
 
+// Score calculates how closely the passed in Whoami matches a candidate Machine.
+// In the current implementation, Score awards points based on the following
+// criteria:
+//
+// * 25 points if the Machine has an SSNHash that matches the one in the Whoami
+//
+// * 25 points if the Machine has a CSNHash that matches the one in the Whoami
+//
+// * 50 points if the Machine has a SystemUUID that matches the one in the Whoami
+//
+// * 0 to 100 points varying depending on how many memory DIMMs from the machine
+//   fingerprint are present in Whoami.
+//
+// * 0 to 100 points varying depending on how many HardwareAddrs from the Machine
+//   are present in Whoami.
+//
+// * 1000 points if the machine UUID matches OnDiskUUID
+//
+// If the score is less than 100 at the end of the scoring process, it is rounded down
+// to zero.  The intent is to be resilient in the face of hardware changes:
+//
+// * SSNHash, CSNHash, and SystemUUID come from the motherboard.
+//
+// * MemoryIds are generated deterministically from the DIMMs installed in the system
+//
+// * MacAddrs comes from the physical Ethernet devices in the system.
 func (w *Whoami) Score(m *Machine) (score int) {
 	if len(m.Fingerprint.SSNHash) > 0 {
 		if bytes.Equal(w.Fingerprint.SSNHash, m.Fingerprint.SSNHash) {
